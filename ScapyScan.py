cf612c67aa09028c7b67787ce2e2fd62c83b4e52
ganye <ganyekins@gmail.com>
2013-12-02 10:35:40 -0800

    working on argparser

--- ScapyScan.py
+++ ScapyScan.py
@@ -1,4 +1,21 @@
+#! /usr/bin/python2.7
+# NOTE: Change the interpreter to correspond to your installation of Python 2.7
+
+import argparse
 from scanner import *
+
+def argparse():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-t", help="Target to scan")
+   parser.add_argument("-sT", help="Performs a TCP Connect scan")
+   parser.add_argument("-sS", help="Performs a TCP Half (Stealth) scan")
+   parser.add_argument("-sX", help="Performs a TCP Xmas scan")
+   parser.add_argument("-sN", help="Performs a TCP Null scan")
+   parser.add_argument("-sF", help="Performs a TCP Fin scan")
+   parser.add_argument("-q", "--quiet", help="Send any output to a log file",
+                       action="log", default=None, type=str)
+   
+   return parser.parse_args()
 
 def map_scanner_arg(arg):
    scanners = {
@@ -9,4 +26,10 @@ def map_scanner_arg(arg):
       'sF'  :  TCPFinScan,
    }
 
-   return scanners[arg]
+   try:
+      return scanners[arg]
+   except KeyError:
+      return None
+
+if __name__ == "__main__":
+   pass
